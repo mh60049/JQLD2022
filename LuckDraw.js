a7dee3076afb8c2c30c860898fb4df04daaaa2f0
mh60049 <44577580+mh60049@users.noreply.github.com>
2022-10-29 14:25:21 +0800

    Add files via upload

--- LuckDraw.js
+++ LuckDraw.js
@@ -9,6 +9,8 @@ let arrWinner = [];
 let arrWinners = [];
 let blnFinished = false;
 let lngWinner = 5;  //Maximum number of winners
+let lngTotalWinner = lngWinner;
+let lngExistingWinner = 0;
 
 
 getData();
@@ -28,11 +30,34 @@ async function getData() {
     userName.className='userName1';
     userID = ('id' + toString(theID));
     userName.id = theID;
-    userName.textContent = `${item.mood}`;
+    userName.textContent = `${item.UserName}`;
 
     theUsers.append(userName);
   }
-  console.log(data);
+  
+  //Load previously drawn winners from localStorage
+  lngExistingWinner = window.localStorage.length;
+  if (lngExistingWinner > 0) {
+    for (lng1 = lngTotalWinner; lng1 > (lngTotalWinner - lngExistingWinner); --lng1) {
+      let strKey = lng1.toString();
+      let strUserName = window.localStorage.getItem(strKey);
+      strWinner=strKey + '. ' + strUserName  //Text to be displayed on winner buttons
+      arrWinners.push(strWinner);  //Add the winner to the array arrWinners
+
+      let btnWinner = document.createElement("button");
+      btnWinner.innerHTML=strWinner;
+      btnWinner.style.textAlign = "left";
+      btnWinner.style.backgroundColor = "green";
+      btnWinner.style.color = "white";
+      btnWinner.style.borderColor = "lightgreen";
+
+      let div1 = document.getElementById("divWinners")
+      div1.appendChild(btnWinner);
+      
+      lngWinner=lngTotalWinner-lngExistingWinner;
+    }
+  }
+
 }
 
 function changeStyle(){
@@ -62,6 +87,20 @@ function changeStyle(){
   }
 }
 
+function clearWinners() {
+  let blnClear=false;
+
+  blnClear=confirm('Would you like to clear winner list?');
+  if (blnClear === true){
+    window.localStorage.clear();
+    let div1 = document.getElementById("divWinners")
+    while (div1.firstChild) {
+      div1.removeChild(div1.firstChild);
+    }
+    lngWinner = lngTotalWinner;
+  }
+}
+
 function startDraw() {
   if (blnFinished === false) {
     if (lngWinner > 0) {
@@ -76,14 +115,17 @@ function startDraw() {
         arrWinner.push(myID.toString());
 
         let strUserName = document.getElementById('txtWinner').textContent;
-        strWinner=lngWinner + '. ' + strUserName
-        arrWinners.push(strWinner);
+        strWinner=lngWinner + '. ' + strUserName  //Text to be displayed on winner buttons
+        arrWinners.push(strWinner);  //Add the winner to the arrange arrWinners
+        window.localStorage.setItem(lngWinner.toString(), strUserName);
+
         let btnWinner = document.createElement("button");
         btnWinner.innerHTML=strWinner;
         btnWinner.style.textAlign = "left";
         btnWinner.style.backgroundColor = "green";
         btnWinner.style.color = "white";
         btnWinner.style.borderColor = "lightgreen";
+        btnWinner.id='btnWin' + lngWinner.toString();
 
         lngWinner = lngWinner - 1;
 
@@ -96,17 +138,41 @@ function startDraw() {
         }
       }
     } else {  
-      /*const allWinners = JSON.stringify(arrWinners);
-      fs.writeFile("./AllWinners.json", allWinners, 'utf8', function (err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("The file for all winners was saved!");
-      })*/
       alert('Lucky draw has been successfully concluded.')
     }
   } else {
-    alert('Nothing left to be drawn.')
+    let blnShowLocalWinners = false;
+    blnShowLocalWinners=confirm('Nothing left to be drawn already. Would you like to show winners?');
+    if (blnShowLocalWinners === true) {
+      showLocalWinners();
+    }
+  }
+}
+
+function showLocalWinners() {
+  let lngTW = window.localStorage.length;
+  let strS='';
+  if (lngTW > 0) {
+      for (lng1 = (lngWinner + 1); lng1 <= lngTotalWinner; ++lng1){
+        let strKey = lng1.toString();
+        strS = strS + '\n' + strKey + '. ' + window.localStorage.getItem(strKey);
+      }
+  }
+  alert('The existing ' + lngTW + ' winners are:' + strS);
+}
+
+function removeLastWinner() {
+  let blnRemove = confirm('Confirm to remove the last winner?');
+  if (blnRemove === true) {
+    strKey = (lngTotalWinner - lngWinner + 2).toString();
+    window.localStorage.removeItem(strKey);
+    arrWinner.pop;
+    arrWinners.pop;
+
+    let div1 = document.getElementById("divWinners")
+    div1.removeChild(div1.lastChild);
+
+    lngWinner = lngWinner+1;
   }
 }
 
